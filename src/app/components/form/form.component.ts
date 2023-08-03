f0cfa118df7fc5384ba73230e4c9bd74e269b51a
radsylph <andresalvarezsilva6@gmail.com>
2023-08-03 17:21:20 -0400

    feat:migrated logic to separate components

--- src/app/components/form/form.component.ts
+++ src/app/components/form/form.component.ts
@@ -1,33 +1,22 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { IndexedDbService } from 'src/app/services/indexdb.service';
-import { Recurso } from 'src/app/models/recurso.model';
-import { InputTextModule } from 'primeng/inputtext';
-import { SelectComponent } from '../select/select.component';
+// import { Recurso } from 'src/app/models/recurso.model';
+// import { RecursoComponent } from '../crud/recurso/recurso.component';
+// import { ProyectoComponent } from '../crud/proyecto/proyecto.component';
+// import { NotificacionComponent } from '../crud/notificacion/notificacion.component';
+// import { Proyecto } from 'src/app/models/proyecto.model';
+// import { Notificacion } from 'src/app/models/notificacion.model';
 import { ActivatedRoute } from '@angular/router';
-import { CalendarModule } from 'primeng/calendar';
-import { Proyecto } from 'src/app/models/proyecto.model';
-import { Notificacion } from 'src/app/models/notificacion.model';
+
+import { Message, MessageService } from 'primeng/api';
 
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
   styleUrls: ['./form.component.css'],
+  providers: [MessageService],
 })
 export class FormComponent {
-  @ViewChild(SelectComponent) selectComponent: SelectComponent | undefined;
-  tablaActual: string = 'Recurso';
-  Crud: string = '';
-  nuevoRecurso: Recurso = { id: 0, nombre: '' };
-  // manualId: number = 0; // Agregar esta variable para el ID manual
-  nuevoProyecto: Proyecto = { id: 0, nombre: '', fechaInicio: new Date() };
-  nuevoNotificacion: Notificacion = {
-    id: 0,
-    fecha: new Date(),
-    proyectoId: 0,
-    tiempoDedicado: 0,
-    recursoId: 0,
-    avance: 0,
-  };
   constructor(
     private indexedDbService: IndexedDbService,
     private route: ActivatedRoute
@@ -37,266 +26,289 @@ export class FormComponent {
     });
   }
 
+  tablaActual: string = 'Recurso';
+  Crud: string = '';
+  // nuevoRecurso: Recurso = { id: 0, nombre: '' };
+  // nuevoProyecto: Proyecto = {
+  //   id: 0,
+  //   nombre: '',
+  //   fechaInicio: new Date(),
+  //   progreso: 0,
+  // };
+  // nuevoNotificacion: Notificacion = {
+  //   id: 0,
+  //   fecha: new Date(),
+  //   proyectoId: 0,
+  //   tiempoDedicado: 0,
+  //   recursoId: 0,
+  //   avance: 0,
+  // };
+
   ngOnInit() {
-    // Leer los parámetros de ruta
     this.route.params.subscribe((params) => {
       const id = params['id'];
       console.log('ID de ruta:', id);
-      // Realiza cualquier acción que necesites con el ID
     });
-
-    // Leer el fragmento de la URL
     this.route.fragment.subscribe((fragment) => {
       console.log('Fragmento:', fragment);
-      // Realiza cualquier acción que necesites con el fragmento
     });
     this.Crud = this.route.snapshot.url.join('/');
     console.log('URL:', this.Crud);
   }
 
-  // cambiarTabla(tabla: string) {
-  //   console.log('Tabla seleccionada:', tabla);
-  //   this.tablaActual = tabla; // Cambiar la tabla actual según la selección
-  //   console.log(this.tablaActual);
+  // agregarRecurso() {
+  //   if (this.nuevoRecurso.nombre.trim() === '') {
+  //     console.warn('Nombre de recurso vacío, no se agrega.');
+  //     this.messageService.add({
+  //       severity: 'error',
+  //       summary: 'Error',
+  //       detail: 'Nombre de recurso vacío, no se agrega.',
+  //     });
+  //     return;
+  //   }
+
+  //   if (this.nuevoRecurso.id === 0) {
+  //     console.warn('ID de recurso vacío, no se agrega.');
+  //     this.messageService.add({
+  //       severity: 'error',
+  //       summary: 'Error',
+  //       detail: 'ID de recurso vacío, no se agrega.',
+  //     }); // Agrega un mensaje de error
+  //     return;
+  //   }
+
+  //   this.indexedDbService
+  //     .addRecurso(this.nuevoRecurso)
+  //     .then(() => {
+  //       console.log('Recurso agregado exitosamente');
+  //       this.messageService.add({
+  //         severity: 'success',
+  //         summary: 'Recurso Agregado',
+  //         detail: 'Recurso agregado exitosamente.',
+  //       }); // Agrega un mensaje de éxito
+  //       this.nuevoRecurso = { id: 0, nombre: '' }; // Limpiar el formulario
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al agregar recurso', error);
+  //     });
   // }
 
-  verDate() {
-    console.log(this.nuevoProyecto.fechaInicio);
-  }
+  // obtenerRecursos() {
+  //   this.indexedDbService
+  //     .getAllRecursos()
+  //     .then((recursos) => {
+  //       console.log('Recursos obtenidos:', recursos);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al obtener recursos', error);
+  //     });
+  // }
 
-  agregarRecurso() {
-    if (this.nuevoRecurso.nombre.trim() === '') {
-      console.warn('Nombre de recurso vacío, no se agrega.');
-      return;
-    }
+  // actualizarRecurso() {
+  //   if (this.nuevoRecurso.id === 0) {
+  //     console.warn('ID de recurso vacío, no se agrega.');
+  //     return;
+  //   }
+  //   this.indexedDbService
+  //     .getRecurso(this.nuevoRecurso.id)
+  //     .then((recurso) => {
+  //       if (!recurso) {
+  //         console.warn(`Recurso con ID ${this.nuevoRecurso.id} no encontrado.`);
+  //         return;
+  //       }
 
-    if (this.nuevoRecurso.id === 0) {
-      console.warn('ID de recurso vacío, no se agrega.');
-      return;
-    }
+  //       recurso.nombre = this.nuevoRecurso.nombre; // Cambia el nombre a lo que necesites
 
-    this.indexedDbService
-      .addRecurso(this.nuevoRecurso)
-      .then(() => {
-        console.log('Recurso agregado exitosamente');
-        this.nuevoRecurso = { id: 0, nombre: '' }; // Limpiar el formulario
-      })
-      .catch((error) => {
-        console.error('Error al agregar recurso', error);
-      });
-  }
+  //       this.indexedDbService
+  //         .updateRecurso(recurso)
+  //         .then(() => {
+  //           console.log('Recurso actualizado exitosamente');
+  //         })
+  //         .catch((error) => {
+  //           console.error('Error al actualizar recurso', error);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al obtener recurso para actualización', error);
+  //     });
+  // }
 
-  obtenerRecursos() {
-    this.indexedDbService
-      .getAllRecursos()
-      .then((recursos) => {
-        console.log('Recursos obtenidos:', recursos);
-      })
-      .catch((error) => {
-        console.error('Error al obtener recursos', error);
-      });
-  }
+  // eliminarRecurso() {
+  //   this.indexedDbService
+  //     .deleteRecurso(this.nuevoRecurso.id)
+  //     .then(() => {
+  //       console.log('Recurso eliminado exitosamente');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al eliminar recurso', error);
+  //     });
+  // }
 
-  actualizarRecurso() {
-    if (this.nuevoRecurso.id === 0) {
-      console.warn('ID de recurso vacío, no se agrega.');
-      return;
-    }
-    this.indexedDbService
-      .getRecurso(this.nuevoRecurso.id)
-      .then((recurso) => {
-        if (!recurso) {
-          console.warn(`Recurso con ID ${this.nuevoRecurso.id} no encontrado.`);
-          return;
-        }
+  // agregarProyecto() {
+  //   if (this.nuevoProyecto.nombre.trim() === '') {
+  //     console.warn('Nombre de proyecto vacío, no se agrega.');
+  //     return;
+  //   }
 
-        recurso.nombre = this.nuevoRecurso.nombre; // Cambia el nombre a lo que necesites
+  //   if (this.nuevoProyecto.id === 0) {
+  //     console.warn('ID de recurso vacío, no se agrega.');
+  //     return;
+  //   }
 
-        this.indexedDbService
-          .updateRecurso(recurso)
-          .then(() => {
-            console.log('Recurso actualizado exitosamente');
-          })
-          .catch((error) => {
-            console.error('Error al actualizar recurso', error);
-          });
-      })
-      .catch((error) => {
-        console.error('Error al obtener recurso para actualización', error);
-      });
-  }
+  //   this.indexedDbService
+  //     .addProyecto(this.nuevoProyecto)
+  //     .then(() => {
+  //       console.log('Proyecto agregado exitosamente');
+  //       this.nuevoProyecto = {
+  //         id: 0,
+  //         nombre: '',
+  //         fechaInicio: new Date(),
+  //         progreso: 0,
+  //       }; // Limpiar el formulario
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al agregar proyecto', error);
+  //     });
+  // }
 
-  eliminarRecurso() {
-    this.indexedDbService
-      .deleteRecurso(this.nuevoRecurso.id)
-      .then(() => {
-        console.log('Recurso eliminado exitosamente');
-      })
-      .catch((error) => {
-        console.error('Error al eliminar recurso', error);
-      });
-  }
+  // obtenerProyectos() {
+  //   this.indexedDbService
+  //     .getAllProyectos()
+  //     .then((proyectos) => {
+  //       console.log('Proyectos obtenidos:', proyectos);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al obtener proyectos', error);
+  //     });
+  // }
 
-  agregarProyecto() {
-    if (this.nuevoProyecto.nombre.trim() === '') {
-      console.warn('Nombre de proyecto vacío, no se agrega.');
-      return;
-    }
+  // actualizarProyecto() {
+  //   this.indexedDbService
+  //     .getProyecto(this.nuevoProyecto.id)
+  //     .then((proyecto) => {
+  //       if (!proyecto) {
+  //         console.warn(
+  //           `Proyecto con ID ${this.nuevoProyecto.id} no encontrado.`
+  //         );
+  //         return;
+  //       }
 
-    if (this.nuevoProyecto.id === 0) {
-      console.warn('ID de recurso vacío, no se agrega.');
-      return;
-    }
+  //       proyecto.nombre = this.nuevoProyecto.nombre; // Cambia el nombre a lo que necesites
 
-    this.indexedDbService
-      .addProyecto(this.nuevoProyecto)
-      .then(() => {
-        console.log('Proyecto agregado exitosamente');
-        this.nuevoProyecto = { id: 0, nombre: '', fechaInicio: new Date() }; // Limpiar el formulario
-      })
-      .catch((error) => {
-        console.error('Error al agregar proyecto', error);
-      });
-  }
+  //       this.indexedDbService
+  //         .updateProyecto(proyecto)
+  //         .then(() => {
+  //           console.log('Proyecto actualizado exitosamente');
+  //         })
+  //         .catch((error) => {
+  //           console.error('Error al actualizar proyecto', error);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al obtener proyecto para actualización', error);
+  //     });
+  // }
 
-  obtenerProyectos() {
-    this.indexedDbService
-      .getAllProyectos()
-      .then((proyectos) => {
-        console.log('Proyectos obtenidos:', proyectos);
-      })
-      .catch((error) => {
-        console.error('Error al obtener proyectos', error);
-      });
-  }
+  // eliminarProyecto() {
+  //   this.indexedDbService
+  //     .deleteProyecto(this.nuevoProyecto.id)
+  //     .then(() => {
+  //       console.log('Proyecto eliminado exitosamente');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al eliminar proyecto', error);
+  //     });
+  // }
 
-  actualizarProyecto() {
-    this.indexedDbService
-      .getProyecto(this.nuevoProyecto.id)
-      .then((proyecto) => {
-        if (!proyecto) {
-          console.warn(
-            `Proyecto con ID ${this.nuevoProyecto.id} no encontrado.`
-          );
-          return;
-        }
+  // agregarNotificacion() {
+  //   if (this.nuevoNotificacion.id === 0) {
+  //     console.warn('ID de notificacion vacío, no se agrega.');
+  //     return;
+  //   }
+  //   if (this.nuevoNotificacion.recursoId === 0) {
+  //     console.warn('ID de recurso vacío, no se agrega.');
+  //     return;
+  //   }
+  //   if (this.nuevoNotificacion.proyectoId === 0) {
+  //     console.warn('ID de proyecto vacío, no se agrega.');
+  //     return;
+  //   }
+  //   if (this.nuevoNotificacion.tiempoDedicado === 0) {
+  //     console.warn('Tiempo dedicado vacío, no se agrega.');
+  //     return;
+  //   }
+  //   if (this.nuevoNotificacion.avance === 0) {
+  //     console.warn('Avance vacío, no se agrega.');
+  //     return;
+  //   }
 
-        proyecto.nombre = this.nuevoProyecto.nombre; // Cambia el nombre a lo que necesites
+  //   this.indexedDbService
+  //     .addNotificacion(this.nuevoNotificacion)
+  //     .then(() => {
+  //       console.log('Notificacion agregada exitosamente');
+  //       this.nuevoNotificacion = {
+  //         id: 0,
+  //         fecha: new Date(),
+  //         proyectoId: 0,
+  //         tiempoDedicado: 0,
+  //         recursoId: 0,
+  //         avance: 0,
+  //       }; // Limpiar el formulario
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al agregar notificacion', error);
+  //     });
+  // }
 
-        this.indexedDbService
-          .updateProyecto(proyecto)
-          .then(() => {
-            console.log('Proyecto actualizado exitosamente');
-          })
-          .catch((error) => {
-            console.error('Error al actualizar proyecto', error);
-          });
-      })
-      .catch((error) => {
-        console.error('Error al obtener proyecto para actualización', error);
-      });
-  }
+  // obtenerNotificaciones() {
+  //   this.indexedDbService
+  //     .getAllNotificaciones()
+  //     .then((notificaciones) => {
+  //       console.log('Notificaciones obtenidas:', notificaciones);
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al obtener notificaciones', error);
+  //     });
+  // }
 
-  eliminarProyecto() {
-    this.indexedDbService
-      .deleteProyecto(this.nuevoProyecto.id)
-      .then(() => {
-        console.log('Proyecto eliminado exitosamente');
-      })
-      .catch((error) => {
-        console.error('Error al eliminar proyecto', error);
-      });
-  }
+  // actualizarNotificacion() {
+  //   this.indexedDbService
+  //     .getNotificacion(this.nuevoNotificacion.id)
+  //     .then((notificacion) => {
+  //       if (!notificacion) {
+  //         console.warn(
+  //           `Notificacion con ID ${this.nuevoNotificacion.id} no encontrada.`
+  //         );
+  //         return;
+  //       }
 
-  agregarNotificacion() {
-    if (this.nuevoNotificacion.id === 0) {
-      console.warn('ID de notificacion vacío, no se agrega.');
-      return;
-    }
-    if (this.nuevoNotificacion.recursoId === 0) {
-      console.warn('ID de recurso vacío, no se agrega.');
-      return;
-    }
-    if (this.nuevoNotificacion.proyectoId === 0) {
-      console.warn('ID de proyecto vacío, no se agrega.');
-      return;
-    }
-    if (this.nuevoNotificacion.tiempoDedicado === 0) {
-      console.warn('Tiempo dedicado vacío, no se agrega.');
-      return;
-    }
-    if (this.nuevoNotificacion.avance === 0) {
-      console.warn('Avance vacío, no se agrega.');
-      return;
-    }
+  //       notificacion.avance = this.nuevoNotificacion.avance; // Cambia el nombre a lo que necesites
 
-    this.indexedDbService
-      .addNotificacion(this.nuevoNotificacion)
-      .then(() => {
-        console.log('Notificacion agregada exitosamente');
-        this.nuevoNotificacion = {
-          id: 0,
-          fecha: new Date(),
-          proyectoId: 0,
-          tiempoDedicado: 0,
-          recursoId: 0,
-          avance: 0,
-        }; // Limpiar el formulario
-      })
-      .catch((error) => {
-        console.error('Error al agregar notificacion', error);
-      });
-  }
+  //       this.indexedDbService
+  //         .updateNotificacion(notificacion)
+  //         .then(() => {
+  //           console.log('Notificacion actualizada exitosamente');
+  //         })
+  //         .catch((error) => {
+  //           console.error('Error al actualizar notificacion', error);
+  //         });
+  //     })
+  //     .catch((error) => {
+  //       console.error(
+  //         'Error al obtener notificacion para actualización',
+  //         error
+  //       );
+  //     });
+  // }
 
-  obtenerNotificaciones() {
-    this.indexedDbService
-      .getAllNotificaciones()
-      .then((notificaciones) => {
-        console.log('Notificaciones obtenidas:', notificaciones);
-      })
-      .catch((error) => {
-        console.error('Error al obtener notificaciones', error);
-      });
-  }
-
-  actualizarNotificacion() {
-    this.indexedDbService
-      .getNotificacion(this.nuevoNotificacion.id)
-      .then((notificacion) => {
-        if (!notificacion) {
-          console.warn(
-            `Notificacion con ID ${this.nuevoNotificacion.id} no encontrada.`
-          );
-          return;
-        }
-
-        notificacion.avance = this.nuevoNotificacion.avance; // Cambia el nombre a lo que necesites
-
-        this.indexedDbService
-          .updateNotificacion(notificacion)
-          .then(() => {
-            console.log('Notificacion actualizada exitosamente');
-          })
-          .catch((error) => {
-            console.error('Error al actualizar notificacion', error);
-          });
-      })
-      .catch((error) => {
-        console.error(
-          'Error al obtener notificacion para actualización',
-          error
-        );
-      });
-  }
-
-  eliminarNotificacion() {
-    this.indexedDbService
-      .deleteNotificacion(this.nuevoNotificacion.id)
-      .then(() => {
-        console.log('Notificacion eliminada exitosamente');
-      })
-      .catch((error) => {
-        console.error('Error al eliminar notificacion', error);
-      });
-  }
+  // eliminarNotificacion() {
+  //   this.indexedDbService
+  //     .deleteNotificacion(this.nuevoNotificacion.id)
+  //     .then(() => {
+  //       console.log('Notificacion eliminada exitosamente');
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error al eliminar notificacion', error);
+  //     });
+  // }
 }
