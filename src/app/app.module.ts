df6be193dc523f873400b152ab71dbde01b53a5d
radsylph <andresalvarezsilva6@gmail.com>
2023-08-03 00:41:58 -0400

    added new imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule } from './app-routing.module'; // tener el AppRoutingModule
 import { AppComponent } from './app.component';
 import { DialogComponent } from './components/dialog/dialog.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
 import { MatButtonModule } from '@angular/material/button';
-import { FormComponent } from './components/form/form.component'; // Corregir esta importación
+import { FormComponent } from './components/form/form.component';
+import { Routes, RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: FormComponent },
+  { path: 'dialog', component: DialogComponent },
+];
 
 @NgModule({
   declarations: [AppComponent, DialogComponent, FormComponent],
@@ -16,7 +26,12 @@ import { FormComponent } from './components/form/form.component'; // Corregir es
     BrowserAnimationsModule,
     MatSlideToggleModule,
     MatButtonModule,
+    RouterModule.forRoot(routes),
+    FormsModule,
+    InputTextModule,
+    ButtonModule,
   ],
+  exports: [RouterModule],
   providers: [],
   bootstrap: [AppComponent],
 })
